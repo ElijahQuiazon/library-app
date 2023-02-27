b0aac96af80de22601808bc6330e8a9561403861
Elijah <elijah.quiazon@gmail.com>
2023-02-27 23:08:32 +1100

    update book instance controller

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,9 +1,20 @@
 // import BookInstance module
 const BookInstance = require("../models/bookinstance");
 
-// display list of all BookInstance
-exports.bookinstance_list = (req, res) => {
-    res.send("NOT IMPLEMENTED: BookInstance list");
+// Display list of all BookInstances.
+exports.bookinstance_list = function (req, res, next) {
+    BookInstance.find()
+        .populate("book")
+        .exec(function (err, list_bookinstances) {
+            if (err) {
+                return next(err);
+            }
+            // Successful, so render
+            res.render("bookinstance_list", {
+                title: "Book Instance List",
+                bookinstance_list: list_bookinstances,
+            });
+        });
 };
 
 // display detail page for a specific BookInstance
